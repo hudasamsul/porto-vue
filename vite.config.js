85e0695cdf3c60912221fe03d3103e80643f2b5e
hudasamsul <samsulaja1804@gmail.com>
2025-06-12 00:39:13 +0700

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/porto-vue/',
+  
   plugins: [
     vue(),
     vueDevTools(),
